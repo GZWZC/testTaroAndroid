f6dfe17bad4250f5082ad5d511c7c35897330657
cc <453873365@qq.com>
2021-05-26 21:48:19 +0800

    first commit

---
+++ config/index.js
@@ -0,0 +1,87 @@
+console.log(`${process.cwd()}/node_modules/@tarojs/components)`)
+const config = {
+  projectName: 'myApp',
+  date: '2021-5-10',
+  designWidth: 750,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
+  plugins: [],
+  defineConstants: {
+  },
+  copy: {
+    patterns: [
+    ],
+    options: {
+    }
+  },
+  framework: 'react',
+  mini: {
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {
+
+        }
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 1024 // 设定转换尺寸上限
+        }
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    }
+  },
+  h5: {
+    publicPath: '/',
+    staticDirectory: 'static',
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {
+        }
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    },
+    webpackChain (chain, webpack) {
+      chain.merge({
+        module: {
+          rules: {
+            myloader: {
+              test: /.(js|jsx|ts|tsx)$/,
+              include:  [`${process.cwd()}/node_modules/@tarojs/components)`, `${process.cwd()}/node_modules/@stencil`],
+              use: [{
+                loader: 'babel-loader',
+                options: {}
+              }]
+            }
+          }
+        }
+      })
+    }
+  }
+}
+
+module.exports = function (merge) {
+  if (process.env.NODE_ENV === 'development') {
+    return merge({}, config, require('./dev'))
+  }
+  return merge({}, config, require('./prod'))
+}
